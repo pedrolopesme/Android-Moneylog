dd0eae759ba123da2d87beefe5c8955af1e61f5d
Pedro Mendes <pedro.mendes@peixeurbano.com>
2018-03-20 22:48:28 -0300

    Creating Notification Util

---
+++ src/app/src/main/java/com/moneylog/android/moneylog/utils/NotificationUtil.java
@@ -0,0 +1,44 @@
+package com.moneylog.android.moneylog.utils;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.moneylog.android.moneylog.R;
+
+/**
+ * NotificationUtil
+ */
+public class NotificationUtil {
+
+    public void openNotification(Context context, Class<?> activity, String title, String message) {
+
+        Intent intent = new Intent(context, activity);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
+
+        b.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.bg_tx_income)
+                .setTicker("MoneyLog")
+                .setContentTitle(title)
+                .setContentText(message)
+                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                .setContentIntent(contentIntent)
+                .setContentInfo("Info");
+
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, b.build());
+
+    }
+
+}
